f3b9a55e903b740019211559e1ddff6bb54c5b98
tkameyamax <tkameyamax@gmail.com>
2019-06-04 13:51:05 +0900

    解答

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,24 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+
+  # total: 飲んだ総数
+  # empty: 手持ちの空き缶の数
+  # まずもらった缶をすべて飲む
+  total = amount
+  empty = amount
+
+  # 空き缶をすべて交換する　交換した缶をすべて飲むを交換できなくなるまで繰り返す
+  while empty > 2
+    # 手持ちの空き缶3つで新たに交換した本数
+    new_drink = (empty / 3)
+    # 飲んだ総数は今まで飲んだ数に新たに交換した数
+    total = total + new_drink
+    # 手持ちの空き缶は新たに交換した缶の数＋交換できなかった缶の数
+    empty = new_drink + (empty % 3)
+  end
+
+  # 今まで飲んだ総数を返す
+  total
+
   end
 end
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -3,3 +3,23 @@ require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 describe BonusDrink do
   specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
 end
+
+describe BonusDrink do
+  specify { expect(BonusDrink.total_count_for(1)).to eq 1 }
+end
+
+describe BonusDrink do
+  specify { expect(BonusDrink.total_count_for(3)).to eq 4 }
+end
+
+describe BonusDrink do
+  specify { expect(BonusDrink.total_count_for(11)).to eq 16 }
+end
+
+describe BonusDrink do
+  specify { expect(BonusDrink.total_count_for(50)).to eq 74 }
+end
+
+describe BonusDrink do
+  specify { expect(BonusDrink.total_count_for(100)).to eq 149 }
+end
